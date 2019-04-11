35311fb0c45ceb134e81afdb9b58acc270d5b985
scwebd <scwebd@gmail.com>
2019-04-11 15:51:43 -0600

    Adding basic MVC example

---
+++ routes/view/index.js
@@ -0,0 +1,29 @@
+var router = require("express").Router();
+var db = require("../../models");
+
+// This route renders the homepage
+router.get("/", function (req, res) {
+    db.Post.findAll({}).then(function(posts) {
+        res.render("index", {
+            msg: "Welcome!",
+            posts: posts
+        });
+    });
+});
+
+// This route renders the individual post page
+router.get("/post/:id", function(req, res) {
+    db.Post.findOne({ where: { id: req.params.id } }).then(function(newPost) {
+        res.render("post", {
+            post: newPost
+        });
+    });
+});
+
+// This route renders the 404
+router.get("*", function (req, res) {
+    res.render("404");
+});
+
+module.exports = router;
+
